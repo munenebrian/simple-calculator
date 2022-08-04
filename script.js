08433832a294600ed759c44993576396c70d71eb
munenebrian <njerubrianmunene@gmail.com>
2022-08-05 01:28:57 +0300

    finished on the javascript, basically the calcultor is ready!

--- script.js
+++ script.js
@@ -50,13 +50,28 @@ class Calculator{
      this.operation = undefined
      this.previousOperand = ''
     }
-    getDisplay(number) {
-        return number
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits =stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDiguts.toLocalString('en', {maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     } 
     updateDisplay(){
         this.currentOperandTextElement.innerText = this.getDisplayNumber (this.currentOperand)
         if (this.operation != null) {
             this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
     }
 }
